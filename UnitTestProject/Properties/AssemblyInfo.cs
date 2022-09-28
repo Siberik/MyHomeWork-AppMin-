de9d6f6851cef47f87a5d07bf3c4de4595979771
Vladislav <lolgagr@gmail.com>
2022-09-28 21:41:09 +0500

    Домашняя работа решена,ура

---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9e700ad8-4bf8-4fc7-966c-dce0fcee4eab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject/TestMinAppClass.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MinApp;
+using System;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class TestMinAppClass
+    {
+        [TestMethod]
+        public void TestMin()
+        {
+            int min;
+            min = Program.Min(3,4,5);
+            Assert.AreEqual(3, min);
+        }
+    }
+}
